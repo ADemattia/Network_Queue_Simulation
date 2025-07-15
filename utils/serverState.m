b6c887f217cc12d42bd6e8e53808c6eaaa7dc38e
ADemattia <127196391+ADemattia@users.noreply.github.com>
2025-07-15 04:24:36 +0200

    Delete utils/serverState.m

--- utils/serverState.m
+++
@@ -1,9 +0,0 @@
-classdef serverState
-    enumeration
-        Free         % il server è libero
-        Waiting      % il server ha completato il servizio, è in attesa di rilasciarlo nella coda
-        Working      % sta servendo un cliente
-        StuckInTraffic % il server ha finito il servizio, ma non può uscire poichè bloccato nel traffico 
-    end
-end
-
